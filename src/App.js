6d51d5a5f1c56a1da14fc103b98bb21daa4bf123
Than <than.jarvis@gmail.com>
2019-10-02 20:53:41 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ class App extends React.Component {
   constructor(){
     super()
     
+    this.dropdown = React.createRef()
+
+
   }
 
-
-
-
-
+  toggleShow = () => {
+    console.log(this.dropdown.current);
+    let {current} = this.dropdown
+    if(current.classList.contains('show-animation')){
+      current.classList.add('hide-animation')
+      current.classList.remove('show-animation')
+    } else{
+      current.classList.add('show-animation')
+      current.classList.remove('hide-animation')
+      
+    }
+  }
 
 
   render(){
@@ -25,7 +36,6 @@ class App extends React.Component {
         </head>
         <div id='background-image'>
           <div id='header'>
-            {/* <i className='fas fa-bars'/> */}
             <p id='header-text'>Start Bootstrap</p>
             <div id='header-button-container'>
               <div className='header-button'>SERVICES</div>
@@ -34,9 +44,20 @@ class App extends React.Component {
               <div className='header-button'>TEAM</div>
               <div className='header-button'>CONTACT</div>
             </div>
-            <button id='dropdown'>MENU</button>
-
+            <button id='dropdown-button'>
+              MENU
+              <i className='fas fa-bars' id='hamburger-icon' onClick={this.toggleShow}/>
+            </button>
           </div>
+
+          <div className='dropdown' ref={this.dropdown}>
+            <div className='dropdown-buttons'>SERVICES</div>
+            <div className='dropdown-buttons'>PORTFOLIO</div>
+            <div className='dropdown-buttons'>ABOUT</div>
+            <div className='dropdown-buttons'>TEAM</div>
+            <div className='dropdown-buttons'>CONTACT</div>
+          </div>
+
           <div id='body'>
             <p id='body-text-one'>Welcome To Our Studio!</p>
             <p id='body-text-two'>IT'S NICE TO MEET YOU</p>
